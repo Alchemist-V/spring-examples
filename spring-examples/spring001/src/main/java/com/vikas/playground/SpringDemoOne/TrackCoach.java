7cc05a65ea392a804518c3578ca877bd62f7bff7
Vikas Rajoria <vrajoria@walmartlabs.com>
2017-11-09 00:35:18 +0530

    refactored repo

--- spring-examples/spring001/src/main/java/com/vikas/playground/SpringDemoOne/TrackCoach.java
+++
@@ -1,9 +0,0 @@
-package com.vikas.playground.SpringDemoOne;
-
-public class TrackCoach implements Coach {
-
-	public String getDailyWorkout() {
-		return "dash 100m run";
-	}
-
-}
--- spring/spring001/src/main/java/com/vikas/playground/SpringDemoOne/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.vikas.playground.SpringDemoOne;
-
-public interface Coach {
-
-	public String getDailyWorkout();
-}
